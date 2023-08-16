d3965890e2d88b9f68e101e51fea137e3d9a2111
DevAch33 <achaibane.ayoub@gmail.com>
2023-08-16 15:51:26 +0100

    Done

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    """
+    Write a function that computes the square value of all integers of a matrix.
+    """
     new_matrix = matrix.copy()
 
     for i in range(len(matrix)):
